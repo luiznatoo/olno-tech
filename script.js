cec835c6d1482add7a3885dc086a29f7e9596e45
Luiz Gabriel <ti@belemriotransportes.com>
2024-03-22 14:57:52 -0300

    segunda versão do site mais eficiente

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true});
+
+
+ScrollReveal().reveal('.area-1', { delay: 100 });
+ScrollReveal().reveal('.area-2', { delay: 100 });
+ScrollReveal().reveal('.area-3', { delay: 100 });
+ScrollReveal().reveal('.area-4', { delay: 100 });
+ScrollReveal().reveal('.area-5', { delay: 100 });
