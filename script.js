6480cd4f4eb71583c6069a347b7b97633dfb6544
Luiz Gabriel <ti@belemriotransportes.com>
2024-03-22 15:36:06 -0300

    corrigindo falhar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 ScrollReveal({ reset: true});
 
 
@@ -6,14 +8,3 @@ ScrollReveal().reveal('.area-2', { delay: 100 });
 ScrollReveal().reveal('.area-3', { delay: 100 });
 ScrollReveal().reveal('.area-4', { delay: 100 });
 ScrollReveal().reveal('.area-5', { delay: 100 });
-
-import moduleA from "library";
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  someFunction();
-});
-
-const someFunction = () => {
-  // uses moduleA
-}
